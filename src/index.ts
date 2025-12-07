c34769073b5f9ba4664a841ddd01607ca449cc3d
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2025-12-07 06:46:17 +0000

    basic sentry setup

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
+import * as Sentry from "@sentry/cloudflare";
 import handlerErrorNotFound from "./handlers/errorNotFound";
 import handlerErrorServer from "./handlers/errorServer";
 import status from "./handlers/status";
@@ -22,4 +23,19 @@ app.route("/subscribers", subscribers);
 app.route("/tickets", tickets);
 app.route("/broadcast", broadcast);
 
-export default app;
+export default Sentry.withSentry((env: Cloudflare.Env) => {
+  const { id: versionId } = env.CF_VERSION_METADATA;
+  return {
+    dsn: "https://e6e2b273e018ce8f05569c395cf9b7a0@o4507649139146752.ingest.de.sentry.io/4510492115796048",
+    release: versionId,
+    // Adds request headers and IP for users, for more info visit:
+    // https://docs.sentry.io/platforms/javascript/guides/cloudflare/configuration/options/#sendDefaultPii
+    sendDefaultPii: true,
+    // Enable logs to be sent to Sentry
+    enableLogs: true,
+    // Set tracesSampleRate to 1.0 to capture 100% of spans for tracing.
+    // Learn more at
+    // https://docs.sentry.io/platforms/javascript/guides/cloudflare/configuration/options/#tracesSampleRate
+    tracesSampleRate: 1.0,
+  };
+}, app);
