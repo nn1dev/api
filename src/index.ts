45978e3f54fcf15872862667dede4c823ec66dcf
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2026-09-25 17:30:38 +0100

    update dependencies

--- src/index.ts
+++ src/index.ts
@@ -28,11 +28,9 @@ const { fetch } = withSentry((env: Cloudflare.Env) => {
   return {
     dsn: "https://e6e2b273e018ce8f05569c395cf9b7a0@o4507649139146752.ingest.de.sentry.io/4510492115796048",
     release: env.SENTRY_RELEASE,
-    // Adds request headers and IP for users, for more info visit:
-    // https://docs.sentry.io/platforms/javascript/guides/cloudflare/configuration/options/#sendDefaultPii
-    sendDefaultPii: true,
-    // Enable logs to be sent to Sentry
-    enableLogs: true,
+    // v11 collects request headers, IP, and other data by default, matching
+    // the old sendDefaultPii: true. See:
+    // https://docs.sentry.io/platforms/javascript/guides/cloudflare/configuration/options/#dataCollection
     // Set tracesSampleRate to 1.0 to capture 100% of spans for tracing.
     // Learn more at
     // https://docs.sentry.io/platforms/javascript/guides/cloudflare/configuration/options/#tracesSampleRate
